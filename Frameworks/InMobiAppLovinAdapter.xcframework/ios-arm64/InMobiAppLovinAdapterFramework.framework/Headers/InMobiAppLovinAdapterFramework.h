e30ae049ba1ad3d976b442a7ffdecdc651d303a1
Tataru Robert <robert.tataru98@gmail.com>
2025-03-17 18:08:50 +0200

    First commit

---
+++ Frameworks/InMobiAppLovinAdapter.xcframework/ios-arm64/InMobiAppLovinAdapterFramework.framework/Headers/InMobiAppLovinAdapterFramework.h
@@ -0,0 +1,18 @@
+//
+//  InMobiAppLovinAdapterFramework.h
+//  InMobiAppLovinAdapterFramework
+//
+//  Created by Robert Tataru on 10.03.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InMobiAppLovinAdapterFramework.
+FOUNDATION_EXPORT double InMobiAppLovinAdapterFrameworkVersionNumber;
+
+//! Project version string for InMobiAppLovinAdapterFramework.
+FOUNDATION_EXPORT const unsigned char InMobiAppLovinAdapterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InMobiAppLovinAdapterFramework/PublicHeader.h>
+
+
